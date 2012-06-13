d598fed55a1969216d536b5293ae017e8250577d
shawnjohnson <sjohnson@axiomega.com>
2012-06-13 10:32:31 -0500

    App works with initial abstracted API

--- nocbot.client/src/main/java/com/axiomega/nocbot/client/App.java
+++ nocbot.client/src/main/java/com/axiomega/nocbot/client/App.java
@@ -1,13 +1,29 @@
 package com.axiomega.nocbot.client;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import com.axiomega.nocbot.sdk.NocbotClient;
+import com.axiomega.nocbot.sdk.session.Account;
+import com.axiomega.nocbot.sdk.session.Call;
+import com.axiomega.nocbot.sdk.session.NocbotRestException;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws NocbotRestException
     {
-        System.out.println( "Hello World!" );
+        NocbotClient client = new NocbotClient();
+        Account mainAccount = client.getAccount();
+        Map<String, String> callParams = new HashMap<String, String>();
+		callParams.put("To", "5105551212"); // Replace with a valid phone number
+		callParams.put("From", "(510) 555-1212"); // Replace with a valid phone
+													// number in your account
+		callParams.put("Url", "http://demo.twilio.com/welcome/voice/");
+		Call call = mainAccount.getCallFactory().create(callParams);
+		System.out.println(call.getSid());
     }
 }
